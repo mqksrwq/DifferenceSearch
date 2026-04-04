7f94b815b7ce74f25f2329753933cc02ce5fb020
mqksrwq <maksim4ikb2006@gmail.com>
2026-04-04 20:56:24 +0300

    fix result file output

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	file1, _ := os.Open("MPRM160D_v7.1.bom")
 	file2, _ := os.Open("выгрузка.txt")
+	fmt.Printf("Файлы '%s' и '%s' загружены", file1.Name(), file2.Name())
+
 	defer file1.Close()
 	defer file2.Close()
 
@@ -16,10 +18,16 @@ func main() {
 	data2 := utils.Parse(file2)
 
 	utils.Search(&data1, &data2)
-	fmt.Println(data1)
-	fmt.Println(data2)
 
-	if err := utils.SaveDifferences("differences.txt", data1, data2); err != nil {
+	if len(data1) != len(data2) {
+		fmt.Printf("\nНайдено несоответствий: %d\n", len(data1)+len(data2))
+	} else {
+		fmt.Println("Файлы идентичны")
+	}
+
+	if err := utils.SaveDifferences("result.txt", data1, data2); err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Результат сохранен в файл result.txt")
+
 }
--- utils/file_editor.go
+++ utils/file_editor.go
@@ -4,19 +4,68 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode/utf8"
 )
 
 func SaveDifferences(filename string, data1, data2 []string) error {
 	content := strings.Builder{}
-	content.WriteString("Уникальные значения из первого файла:\n")
-	content.WriteString(strings.Join(data1, "\n"))
-	content.WriteString("\n\nУникальные значения из второго файла:\n")
-	content.WriteString(strings.Join(data2, "\n"))
-	content.WriteString("\n")
+	leftHeader := "MPRM160D_v7.1.bom"
+	rightHeader := "выгрузка.txt"
+
+	if len(data1) == 0 && len(data2) == 0 {
+		if err := os.WriteFile(filename, []byte{}, 0644); err != nil {
+			return fmt.Errorf("error writing differences file: %w", err)
+		}
+		return nil
+	}
+
+	leftWidth := maxColumnWidth(leftHeader, data1)
+	rightWidth := maxColumnWidth(rightHeader, data2)
+	content.WriteString(fmt.Sprintf("%s | %s\n", centerText(leftHeader, leftWidth), centerText(rightHeader, rightWidth)))
+
+	maxLen := len(data1)
+	if len(data2) > maxLen {
+		maxLen = len(data2)
+	}
+
+	for i := 0; i < maxLen; i++ {
+		var left, right string
+		if i < len(data1) {
+			left = data1[i]
+		}
+		if i < len(data2) {
+			right = data2[i]
+		}
+		content.WriteString(fmt.Sprintf("%s | %s\n", centerText(left, leftWidth), centerText(right, rightWidth)))
+	}
 
 	if err := os.WriteFile(filename, []byte(content.String()), 0644); err != nil {
 		return fmt.Errorf("error writing differences file: %w", err)
 	}
 
 	return nil
+}
+
+func maxColumnWidth(header string, data []string) int {
+	width := utf8.RuneCountInString(header)
+	for _, elem := range data {
+		elemWidth := utf8.RuneCountInString(elem)
+		if elemWidth > width {
+			width = elemWidth
+		}
+	}
+	return width + 6
+}
+
+func centerText(text string, width int) string {
+	textWidth := utf8.RuneCountInString(text)
+	if textWidth >= width {
+		return text
+	}
+
+	padding := width - textWidth
+	left := padding / 2
+	right := padding - left
+
+	return strings.Repeat(" ", left) + text + strings.Repeat(" ", right)
 }
